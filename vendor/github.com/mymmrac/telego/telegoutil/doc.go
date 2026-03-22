89637b1d29b3156f1c12880fa81bfb8decdb4d3b
Tilman Baumann <tilman@baumann.name>
2026-03-22 22:40:28 +0100

    Nix needs either a dependency hash or vendor files

---
+++ vendor/github.com/mymmrac/telego/telegoutil/doc.go
@@ -0,0 +1,14 @@
+/*
+Package telegoutil provides utility methods for Telego.
+
+Those utility methods provide a convenient way of construction Telegram methods parameters and other types.
+
+Utilities by files:
+* api.go     - low-level API of Telego
+* methods.go - Telegram methods parameters
+* types.go   - types used in methods parameters
+* handler.go - handler and predicate helpers
+
+Dev Note: This package is designed to be self-contained, and other packages of Telego should not depend on utilities.
+*/
+package telegoutil
